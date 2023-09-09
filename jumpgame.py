64717adb3ecc6d14a7c6fb40defc00b00b986e47
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-08 19:18:59 -0700

    Create jumpgame.py

---
+++ jumpgame.py
@@ -0,0 +1,12 @@
+# 55. Jump Game
+
+class Solution(object):
+    def canJump(self, nums):
+        curr = nums[0]
+        for i in range(1,len(nums)):
+            if curr == 0:
+                return False
+            curr -= 1.
+            curr = max(curr, nums[i])       
+        return True
+        
