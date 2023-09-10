f53b5d3f39606beac85f80cade0f10ea58a451a5
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-10 11:12:22 -0700

    Create summaryranges.py

---
+++ summaryranges.py
@@ -0,0 +1,19 @@
+# 228. Summary Ranges
+
+class Solution:
+  def summaryRanges(self, nums):
+    ans = []
+
+    i = 0
+    while i < len(nums):
+      begin = nums[i]
+      while i < len(nums) - 1 and nums[i] == nums[i + 1] - 1:
+        i += 1
+      end = nums[i]
+      if begin == end:
+        ans.append(str(begin))
+      else:
+        ans.append(str(begin) + "->" + str(end))
+      i += 1
+
+    return ans
