12f6b59be6240677e4b0b03e332c0fcff746f037
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-11 14:53:22 -0700

    Create numberofislands.py

---
+++ numberofislands.py
@@ -0,0 +1,27 @@
+# 200. Number of Islands
+
+class Solution:
+    def numIslands(self, grid):
+        def bfs(i,j):
+            queue=deque()
+            queue.append((i,j))
+            visited.add((i,j))
+            while queue:
+                r,c=queue.popleft()
+                directions=[(0,1),(0,-1),(1,0),(-1,0)]
+                for dr,dc in directions:
+                    row,col=r+dr,c+dc
+                    if row in range(m) and col in range(n) and grid[row][col]=='1' and (row,col) not in visited:
+                        queue.append((row,col))
+                        visited.add((row,col))
+
+        m,n=len(grid),len(grid[0])
+        visited=set()
+        ans=0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]=='1' and (i,j) not in visited:
+                    ans+=1
+                    bfs(i,j)
+        return ans
+
