ec1a3c0b9b6241ebd12396b1143903cf3dfb1da6
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-11 14:55:09 -0700

    Create clonegraph.py

---
+++ clonegraph.py
@@ -0,0 +1,17 @@
+# 133. Clone Graph
+
+class Solution:
+    def cloneGraph(self, node):
+        oldtonew = {}
+        return self.dfs(node, oldtonew)
+
+    def dfs(self, node, oldtonew):
+        if not node:
+            return None
+        if node in oldtonew:
+            return oldtonew[node]
+        copy = Node(node.val)
+        oldtonew[node] = copy
+        for n in node.neighbors:
+            copy.neighbors.append(self.dfs(n, oldtonew))
+        return copy
