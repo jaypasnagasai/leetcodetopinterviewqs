8dd87daa6bb314cc893b06544e504a5c28459fb2
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-11 21:44:04 -0700

    Create sortlist.py

---
+++ sortlist.py
@@ -0,0 +1,20 @@
+# 148. Sort List
+
+class Solution(object):
+    def sortList(self, head):
+        if head is None:
+            return None
+        ptr=head
+        arr=[]
+        while ptr is not None:
+            arr.append(ptr.val)
+            ptr=ptr.next
+        arr.sort()
+        n = ListNode(arr[0])
+        head=n
+        temp=head
+        for i in range(1,len(arr)):
+            n1 = ListNode(arr[i])
+            temp.next=n1
+            temp=temp.next       
+        return head
