312130d066811fd104b74cc0ba2275cb76c88e17
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-12 12:44:03 -0700

    Create plusone.py

---
+++ plusone.py
@@ -0,0 +1,11 @@
+# 66. Plus One
+
+class Solution:
+    def plusOne(self, digits):
+        strings = ""
+        for number in digits:
+            strings += str(number)
+
+        temp = str(int(strings) +1)
+
+        return [int(temp[i]) for i in range(len(temp))]
