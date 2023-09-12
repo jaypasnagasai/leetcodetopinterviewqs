3c515762144983dd9a4bb5577218b472b7f90a2c
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-12 12:16:18 -0700

    Create kpairswithsmallestsum.py

---
+++ kpairswithsmallestsum.py
@@ -0,0 +1,28 @@
+# 373. Find K Pairs with Smallest Sums
+
+import heapq
+
+class Solution:
+    def kSmallestPairs(self, nums1, nums2, k):
+        resV = []  # Result list to store the pairs
+        pq = []  # Priority queue to store pairs with smallest sums, sorted by the sum
+
+        # Push the initial pairs into the priority queue
+        for x in nums1:
+            heapq.heappush(pq, [x + nums2[0], 0])  # The sum and the index of the second element in nums2
+
+        # Pop the k smallest pairs from the priority queue
+        while k > 0 and pq:
+            pair = heapq.heappop(pq)
+            sum_val, pos = pair[0], pair[1]  # Get the smallest sum and the index of the second element in nums2
+
+            currentPair = [sum_val - nums2[pos], nums2[pos]]
+            resV.append(currentPair)  # Add the pair to the result list
+
+            # If there are more elements in nums2, push the next pair into the priority queue
+            if pos + 1 < len(nums2):
+                heapq.heappush(pq, [sum_val - nums2[pos] + nums2[pos + 1], pos + 1])
+
+            k -= 1  # Decrement k
+
+        return resV  # Return the k smallest pairs
