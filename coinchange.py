abb354c8cb609fdde9527b8c789d6437049d6408
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-12 12:58:48 -0700

    Create coinchange.py

---
+++ coinchange.py
@@ -0,0 +1,11 @@
+# 322. Coin Change
+
+class Solution:
+  def coinChange(self, coins, amount):
+    dp = [0] + [amount + 1] * amount
+
+    for coin in coins:
+      for i in range(coin, amount + 1):
+        dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return -1 if dp[amount] == amount + 1 else dp[amount]
