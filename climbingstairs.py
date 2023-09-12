fec42b79da92d3a78baae9c41f2289585ddb3cb0
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-12 12:53:21 -0700

    Create climbingstairs.py

---
+++ climbingstairs.py
@@ -0,0 +1,11 @@
+# 70. Climbing Stairs
+
+class Solution(object):
+    def climbStairs(self, n):
+        k1=0
+        k2=1
+        while n>0:
+            k1,k2=k2,k1+k2
+            n-=1
+
+        return k2
