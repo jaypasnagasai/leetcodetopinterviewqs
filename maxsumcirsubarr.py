9e45dd0e592ad854860d3f23c9f2952e122b6972
jaypasnagasai <75296693+jaypasnagasai@users.noreply.github.com>
2023-09-11 21:50:11 -0700

    Create maxsumcirsubarr.py

---
+++ maxsumcirsubarr.py
@@ -0,0 +1,11 @@
+# 918. Maximum Sum Circular Subarray
+
+class Solution:
+    def maxSubarraySumCircular(self, nums):
+        curmax,curmin,mas,mis=0,0,nums[0],nums[0]
+        for i in nums:
+            curmax=max(curmax+i,i)
+            mas=max(mas,curmax)
+            curmin=min(curmin+i,i)
+            mis=min(mis,curmin)
+        return max(mas,sum(nums)-mis) if mas>0 else mas
